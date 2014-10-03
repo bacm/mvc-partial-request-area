325692c1afba542e7fb8dcd767d6bee67ac12141
bacm <bacm@live.co.uk>
2014-10-04 00:04:56 +0200

    first commit

---
+++ mvc-partial-areas/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mvc_partial_areas.Startup))]
+namespace mvc_partial_areas
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
